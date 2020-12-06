0022a1d96f95fe280e159f59bc44c0a47f203470
Antti Tapio <antti.tapio@iki.fi>
2020-12-06 10:36:38 +0200

    2020/day06

---
+++ 2020/day06/part1/day06part1.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+type CustomsGroup struct {
+	Persons int
+	Answers map[string]bool
+}
+
+func NewCustomsGroup() *CustomsGroup {
+	cg := &CustomsGroup{
+		Answers: map[string]bool{},
+	}
+
+	return cg
+}
+
+func (g *CustomsGroup) AnswerCount() int {
+	count := 0
+	for _, v := range g.Answers {
+		if v {
+			count++
+		}
+	}
+
+	return count
+}
+
+func (g *CustomsGroup) AddPerson(data string) {
+	answers := strings.Split(data, "")
+	for _, a := range answers {
+		g.Answers[a] = true
+	}
+}
+
+type data struct {
+	input []*CustomsGroup
+}
+
+func findAnswer(d data) int {
+	count := 0
+
+	for _, g := range d.input {
+		count = count + g.AnswerCount()
+	}
+
+	return count
+}
+
+func processInput(lines []string) data {
+	d := data{}
+
+	cg := NewCustomsGroup()
+	d.input = append(d.input, cg)
+
+	for _, line := range lines {
+		switch line {
+		case "":
+			cg = NewCustomsGroup()
+			d.input = append(d.input, cg)
+		default:
+			cg.AddPerson(line)
+		}
+	}
+
+	return d
+}
+
+func main() {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	input := []string{}
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		l := scanner.Text()
+		input = append(input, l)
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	d := processInput(input)
+
+	answer := findAnswer(d)
+
+	fmt.Printf("answer: %d\n", answer)
+}
---
+++ 2020/day06/part1/day06part1_test.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFindAnswer(t *testing.T) {
+	t.Parallel()
+
+	testCases := []struct {
+		Name  string
+		Input []string
+
+		Expected int
+	}{
+		{
+			Name: "example1",
+			Input: []string{
+				"abcx",
+				"abcy",
+				"abcz",
+			},
+			Expected: 6,
+		},
+		{
+			Name: "example1",
+			Input: []string{
+				"abc",
+				"",
+				"a",
+				"b",
+				"c",
+				"",
+				"ab",
+				"ac",
+				"",
+				"a",
+				"a",
+				"a",
+				"a",
+				"",
+				"b",
+			},
+			Expected: 11,
+		},
+	}
+
+	for i := range testCases {
+		tc := testCases[i]
+		t.Run(tc.Name, func(t *testing.T) {
+			t.Parallel()
+
+			d := processInput(tc.Input)
+			got := findAnswer(d)
+
+			assert.Equal(t, tc.Expected, got)
+
+		})
+	}
+}
