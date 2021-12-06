ebaaff0a1ed59a1c3bab83855c78f894515c05a2
Antti Tapio <antti.tapio@iki.fi>
2021-12-06 09:33:51 +0200

    2021/day06

---
+++ 2021/day06/part1/main.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+const MaxCycle = 8
+const CycleStart = 6
+
+type Population struct {
+	cycle []int
+	day   int
+}
+
+func NewPopulation() *Population {
+	return &Population{
+		cycle: make([]int, MaxCycle+1),
+	}
+}
+
+func (p *Population) NextDay() {
+	p.day++
+
+	today, rest := p.cycle[0], p.cycle[1:]
+
+	p.cycle = rest
+	for len(p.cycle) < MaxCycle+1 {
+		p.cycle = append(p.cycle, 0)
+	}
+
+	p.cycle[CycleStart] += today
+	p.cycle[MaxCycle] += today
+}
+
+func (p *Population) Count() int {
+	c := 0
+	for _, dc := range p.cycle {
+		c += dc
+	}
+	return c
+}
+
+func findAnswer(lines []string) (int, error) {
+	population := NewPopulation()
+
+	for _, day := range strings.Split(lines[0], ",") {
+		d, err := strconv.Atoi(day)
+		if err != nil {
+			return 0, err
+		}
+		population.cycle[d]++
+	}
+
+	for i := 0; i < 80; i++ {
+		population.NextDay()
+	}
+
+	return population.Count(), nil
+}
+
+func parseFile(filename string) ([]string, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return []string{}, err
+	}
+	defer file.Close()
+
+	input := []string{}
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		l := scanner.Text()
+		input = append(input, l)
+	}
+
+	if err := scanner.Err(); err != nil {
+		return []string{}, err
+	}
+	return input, nil
+}
+
+func main() {
+	input, err := parseFile("input")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	answer, err := findAnswer(input)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("answer: %d\n", answer)
+}
---
+++ 2021/day06/part2/main_test.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestFindAnswer(t *testing.T) {
+	//t.Parallel()
+	testCases := []struct {
+		Name  string
+		Input []string
+
+		Expected int
+	}{
+		{
+			Name:     "../example1",
+			Input:    []string{"3,4,3,1,2"},
+			Expected: 26984457539,
+		},
+	}
+
+	for i := range testCases {
+		tc := testCases[i]
+		t.Run(tc.Name, func(t *testing.T) {
+			//t.Parallel()
+			input := tc.Input
+
+			if len(input) == 0 {
+				var err error
+				input, err = parseFile(fmt.Sprintf("%s.txt", tc.Name))
+				require.NoError(t, err)
+			}
+
+			got, err := findAnswer(input)
+			require.NoError(t, err)
+
+			assert.Equal(t, tc.Expected, got)
+
+		})
+	}
+}
